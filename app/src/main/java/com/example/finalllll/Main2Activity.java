4c850c64c48b38b00e6138e0c80f661ffa753e1e
yanyanchampaign <yyan24@illinois.edu>
2019-04-30 01:48:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/finalllll/Main2Activity.java
@@ -0,0 +1,83 @@
+package com.example.finalllll;
+
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //beijing
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/282/");
+            }
+        });
+
+        //guangzhou
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/322/");
+            }
+        });
+
+        //shanghai
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/272/");
+            }
+        });
+
+        //shenzhen
+        findViewById(R.id.button6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/1056/");
+            }
+        });
+
+        //chengdu
+        findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/1056/");
+            }
+        });
+
+        //hangzhou
+        findViewById(R.id.button8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("http://www.yohomars.com/yohomars/seo/topic/286/");
+            }
+        });
+
+        //google map
+        findViewById(R.id.button10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("https://www.google.com/maps");
+            }
+        });
+    }
+
+    public void clicked_btn(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+}
